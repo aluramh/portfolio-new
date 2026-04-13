248ad8e654002445ecceeb732a883d8839629f0a
Alex Ramirez <aluramh@gmail.com>
2026-04-13 16:55:01 +0000

    fix: remove enableColorScheme that was overriding light mode theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="system"
           enableSystem
-          enableColorScheme
         >
           {children}
         </ThemeProvider>
